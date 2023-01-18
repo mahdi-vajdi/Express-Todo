69c67909dd0669055fedeeca213fc07a56c5fcb1
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-01-18 10:38:06 +0330

    Setup a skeleton server with database and a route to categories with CRUD actions

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
+
+// Setup database
+mongoose.set("strictQuery", false);
+const mongodbUri = "mongodb://127.0.0.1:27017/SimpleTodo";
+
+mongoose
+  .connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+app.use("/api/categories", require("./routes/category"));
+
+app.use((err, req, res) => {
+  res.status(422).json({ error: err.message });
+});
