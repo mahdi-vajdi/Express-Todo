e1da979321d09f9854ff5068893f354598e980fb
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-01-30 22:49:35 +0330

    refactore: Refactored the category and tak controller: using async instead of callbacks, better responses etc.

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,51 +1,79 @@
+const { default: mongoose } = require("mongoose");
 const Category = require("../model/Category");
 
-const getAllCategories = (req, res, next) => {
-  const page = req.query.p;
-  const itemPerPage = 10;
+const createCategory = async (req, res) => {
+  if (!req?.body?.title)
+    return res.status(400).json({ message: "Category title is required." });
 
-  // let categories = [];
-
-  Category.find()
-    /* .skip(page * itemPerPage)
-      .limit(itemPerPage)
-      .forEach((category) => categories.push(category)) */
-
-    .then((categories) => {
-      res.status(200).json(categories);
-    })
-    .catch(() => {
-      res.status(500).json({ error: "Could not fetch the categories" });
+  try {
+    const category = await Category.create(req.body);
+    return res.status(201).json({
+      message: "Successfully created the category",
+      category,
     });
+  } catch (err) {
+    return res.status(500).json({ error: "Could not create category" });
+  }
 };
 
-const getCategory = (req, res) => {
-  Category.findById({ _id: req.params.id }, req.body).then((category) => {
-    res.json(category);
-  });
+const getAllCategories = async (req, res) => {
+  const categories = await Category.find();
+  if (!categories || categories.length === 0)
+    return res.status(404).json({ message: "There is no categories!" });
+
+  res.status(200).json(categories);
 };
 
-const updateCategory = (req, res, next) => {
-  Category.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
-    Category.findOne({ _id: req.params.id }).then((category) => {
-      res.json(category);
-    });
-  });
+const getCategory = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
+
+  const category = await Category.findById({ _id: req.params.id }, req.body);
+  if (!category)
+    return res
+      .status(404)
+      .json({ message: "No category matches the provided ID" });
+
+  res.status(200).json(category);
 };
 
-const createCategory = (req, res, next) => {
-  Category.create(req.body, (err, doc) => {
-    if (err) {
-      res.status(400).json(err);
-    }
-    res.json(doc._id);
-  });
+const updateCategory = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
+
+  const category = await Category.findByIdAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { returnDocument: "after" }
+  );
+
+  if (!category)
+    return res.status(404).json({ message: "No task matches the provided ID" });
+
+  res
+    .status(200)
+    .json({ message: "Successfully edited the category", category });
 };
 
-const deleteCategory = (req, res, next) => {
-  Category.findByIdAndRemove({ _id: req.params.id }).then((category) => {
-    res.json(category);
-  });
+const deleteCategory = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
+
+  const category = await Category.findByIdAndRemove({ _id: req.params.id });
+  if (!category)
+    return res
+      .status(404)
+      .json({ message: "No category matches the provided ID" });
+
+  res
+    .status(200)
+    .json({ message: "Successfully deleted the category", category });
 };
 
 module.exports = {
--- controller/taskController.js
+++ controller/taskController.js
@@ -1,57 +1,71 @@
+const { default: mongoose } = require("mongoose");
 const Task = require("../model/Task");
 
-const createTask = (req, res) => {
-  Task.create(req.body, (err, doc) => {
-    if (err) {
-      res.status(400).json(err);
-    } else {
-      res.status(200).json(doc);
-    }
+const createTask = async (req, res) => {
+  if (!req?.body?.category || !req?.body?.title)
+    return res
+      .status(400)
+      .json({ message: "category id and task title are required." });
+
+  try {
+    const result = await Task.create(req.body);
+    res
+      .status(201)
+      .json({ message: "Successfully created the task", task: result });
+  } catch (err) {
+    res.status(500).json({ message: "Could not create the task", error: err });
+  }
+};
+
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find();
+  if (!tasks || tasks.length === 0)
+    return res.status(404).json({ message: "There is no tasks" });
+  res.status(200).json(tasks);
+};
+
+const getTask = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
+
+  const task = await Task.findById({ _id: req.params.id }, req.body);
+  if (!task)
+    return res.status(404).json({ message: "No task matches the provided ID" });
+  res.status(200).json(task);
+};
+
+const updateTask = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
+
+  const task = await Task.findByIdAndUpdate({ _id: req.params.id }, req.body, {
+    returnDocument: "after",
+    runValidators: true,
+  });
+  if (!task)
+    return res.status(404).json({ message: "No task matches the provided ID" });
+  res.status(200).json({
+    message: "Successfully updated the task",
+    task,
   });
 };
 
-const getAllTasks = (req, res) => {
-  Task.find((err, docs) => {
-    if (err) {
-      res.status(500).json({ error: "Could not fetch the tasks" });
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-};
+const deleteTask = async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id))
+    return res
+      .status(400)
+      .json({ message: "The requested ID does not have a correct form" });
 
-const getTask = (req, res) => {
-  Task.findById({ _id: req.params.id }, req.body, (err, doc) => {
-    if (err) {
-      res.status(400).json(err);
-    } else {
-      res.status(200).json(doc);
-    }
-  });
-};
-
-const updateTask = (req, res) => {
-  Task.findByIdAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { returnDocument: "after" },
-    (err, doc) => {
-      if (err) {
-        res.status(400).json(err);
-      } else {
-        res.status(200).json(doc);
-      }
-    }
-  );
-};
-
-const deleteTask = (req, res) => {
-  Task.findByIdAndDelete({ _id: req.params.id }, (err, doc) => {
-    if (err) {
-      res.status(400).json(err);
-    } else {
-      res.status(200).json(doc);
-    }
+  const task = await Task.findByIdAndDelete({ _id: req.params.id });
+  if (!task)
+    return res.status(404).json({ message: "No task matches the provided ID" });
+  res.status(200).json({
+    message: "Successfully deleted the task",
+    task,
   });
 };
 
